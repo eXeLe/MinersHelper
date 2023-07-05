2faeab46351368a651535303a188c7c5f378e9eb
Timofey <morozovtg@it.mos.ru>
2023-07-06 01:21:28 +0300

    more content, better timers, dark theme

--- MinersHelper/Program.cs
+++ MinersHelper/Program.cs
@@ -1,15 +1,26 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MinersHelper;
+using MudBlazor;
 using MudBlazor.Services;
 using MudExtensions.Services;
+using Blazored.LocalStorage;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudExtensions();
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
 
+    config.SnackbarConfiguration.VisibleStateDuration = 60000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+   
+});
 await builder.Build().RunAsync();
